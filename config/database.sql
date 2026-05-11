ca014abe746bb4a4bbda954d0403e0293c17534e
Eline <elinevermeulen06@gmail.com>
2026-05-11 22:14:15 +0200

    update du sql de la base de donnée

--- config/database.sql
+++ config/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.3
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 11 mai 2026 à 15:18
--- Version du serveur : 9.1.0
--- Version de PHP : 8.3.14
+-- Généré le : lun. 11 mai 2026 à 20:13
+-- Version du serveur : 8.4.7
+-- Version de PHP : 8.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,23 +42,24 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `id_author` int NOT NULL,
   PRIMARY KEY (`id_article`),
   KEY `fk_articles_users1_idx` (`id_author`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `articles`
 --
 
 INSERT INTO `articles` (`id_article`, `title`, `intro`, `description`, `note`, `critic`, `opinion`, `banner_img`, `card_img`, `date_add`, `id_author`) VALUES
-(1, 'Cyberpunk 2077', 'Un RPG futuriste immersif', 'Description complète du jeu Cyberpunk...', 8.5, 'Critique professionnelle...', 'Mon avis perso...', 'cyberpunk_banner.jpg', 'cyberpunk_card.jpg', '2026-05-04 15:43:58', 1),
-(2, 'The Witcher 3', 'Une aventure épique', 'Description complète de Witcher...', 9.5, 'Chef-d’œuvre du RPG...', 'Incroyable expérience...', 'witcher_banner.jpg', 'witcher_card.jpg', '2026-05-04 15:43:58', 1),
-(3, 'Elden Ring', 'Un monde ouvert exigeant', 'Description complète de Elden Ring...', 9.0, 'Un défi à la hauteur...', 'Très prenant...', 'elden_banner.jpg', 'elden_card.jpg', '2026-05-04 15:43:58', 1),
-(4, 'Red Dead Redemption 2', 'Western réaliste', 'Description complète RDR2...', 9.7, 'Narration exceptionnelle...', 'Magnifique...', 'rdr2_banner.jpg', 'rdr2_card.jpg', '2026-05-04 15:43:58', 1),
-(5, 'Hogwarts Legacy', 'Plongée dans Harry Potter', 'Description Hogwarts...', 7.5, 'Bon mais imparfait...', 'Sympa à jouer...', 'hogwarts_banner.jpg', 'hogwarts_card.jpg', '2026-05-04 15:43:58', 1),
-(6, 'Assassin’s Creed Valhalla', 'Raid viking', 'Description AC Valhalla...', 7.8, 'Un peu répétitif...', 'Bon jeu...', 'ac_banner.jpg', 'ac_card.jpg', '2026-05-04 15:43:58', 1),
-(7, 'Zelda Breath of the Wild', 'Exploration libre', 'Description Zelda...', 9.8, 'Révolution du genre...', 'Exceptionnel...', 'zelda_banner.jpg', 'zelda_card.jpg', '2026-05-04 15:43:58', 1),
-(8, 'God of War Ragnarok', 'Mythologie nordique', 'Description GOW...', 9.6, 'Très cinématographique...', 'Top...', 'gow_banner.jpg', 'gow_card.jpg', '2026-05-04 15:43:58', 1),
-(9, 'Final Fantasy XVI', 'RPG moderne', 'Description FF16...', 8.2, 'Bon système de combat...', 'Sympa...', 'ff_banner.jpg', 'ff_card.jpg', '2026-05-04 15:43:58', 1),
-(10, 'Spider-Man 2', 'Super-héros dynamique', 'Description Spiderman...', 8.9, 'Très fun...', 'Excellent gameplay...', 'spiderman_banner.jpg', 'spiderman_card.jpg', '2026-05-04 15:43:58', 1);
+(1, 'Cyberpunk 2077', 'Un RPG futuriste immersif', 'Description complète du jeu Cyberpunk... Ya des voitures et des modifications corporelles', 8.0, 'Critique professionnelle... je tente encore une modif j\'ai baissé un peu la note', 'Mon avis perso... j\'ai baissé un peu la note parce que j\'avais envie', 'cyberpunk_banner.jpg', 'cyberpunk_card.jpg', '2026-05-04 15:43:58', 1),
+(2, 'The Witcher 3', 'Une aventure épique', 'Description complète de Witcher...', 9.5, 'Chef-d’œuvre du RPG...', 'Incroyable expérience...', 'witcher_banner.jpg', 'witcher_card.jpg', '2026-05-03 15:43:58', 1),
+(3, 'Elden Ring', 'Un monde ouvert exigeant', 'Description complète de Elden Ring...', 9.0, 'Un défi à la hauteur...', 'Très prenant...', 'elden_banner.jpg', 'elden_card.jpg', '2026-03-16 16:43:58', 1),
+(4, 'Red Dead Redemption 2', 'Western réaliste', 'Description complète RDR2...', 9.7, 'Narration exceptionnelle...', 'Magnifique...', 'rdr2_banner.jpg', 'rdr2_card.jpg', '2026-04-21 15:43:58', 1),
+(5, 'Hogwarts Legacy', 'Plongée dans Harry Potter', 'Description Hogwarts...', 7.5, 'Bon mais imparfait...', 'Sympa à jouer...', 'hogwarts_banner.jpg', 'hogwarts_card.jpg', '2026-04-25 15:43:58', 1),
+(6, 'Assassin’s Creed Valhalla', 'Raid viking', 'Description AC Valhalla...', 7.8, 'Un peu répétitif...', 'Bon jeu...', 'ac_banner.jpg', 'ac_card.jpg', '2026-05-01 15:43:58', 1),
+(7, 'Zelda Breath of the Wild', 'Exploration libre', 'Description Zelda...', 9.8, 'Révolution du genre...', 'Exceptionnel...', 'zelda_banner.jpg', 'zelda_card.jpg', '2026-04-30 15:43:58', 1),
+(8, 'God of War Ragnarok', 'Mythologie nordique', 'Description GOW...', 9.6, 'Très cinématographique...', 'Top...', 'gow_banner.jpg', 'gow_card.jpg', '2026-04-28 15:43:58', 1),
+(9, 'Final Fantasy XVI', 'RPG moderne', 'Description FF16...', 8.2, 'Bon système de combat...', 'Sympa...', 'ff_banner.jpg', 'ff_card.jpg', '2026-04-22 15:43:58', 1),
+(10, 'Spider-Man 2', 'Super-héros dynamique', 'Description Spiderman...', 8.9, 'Très fun...', 'Excellent gameplay...', 'spiderman_banner.jpg', 'spiderman_card.jpg', '2026-05-02 15:43:58', 1),
+(12, 'qergser', 'sergser', 'sergserg', 0.2, 'sergsergs', 'sergsergsergsjdtrh', '6a0237fbb1c32.jpg', '6a0237fbb20ce.jpg', '2026-05-11 20:11:39', 1);
 
 -- --------------------------------------------------------
 
@@ -140,4 +141,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
